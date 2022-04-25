679a933f95b80d8845bfa67cb35d7ed6976cc087
fardosa-ibrahim <fardosaibrahimshide@gmail.com>
2022-04-25 16:27:14 +0300

    added a another function

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -24,4 +24,28 @@ fun main(args: Array<String>) {
     println("Consonants: $consonants")
     println("Digits: $digits")
     println("White spaces: $spaces")
+    another()
+}
+fun another() {
+    var line = "This website is aw3som3."
+    var vowels = 0
+    var consonants = 0
+    var digits = 0
+    var spaces = 0
+
+    line = line.toLowerCase()
+    for (i in 0..line.length - 1) {
+        val ch = line[i]
+        when (ch) {
+            'a', 'e', 'i', 'o', 'u' -> ++vowels
+            in 'a'..'z' -> ++consonants
+            in '0'..'9' -> ++digits
+            ' ' -> ++spaces
+        }
+    }
+
+    println("Vowels: $vowels")
+    println("Consonants: $consonants")
+    println("Digits: $digits")
+    println("White spaces: $spaces")
 }
